ca710764c5692b5ad4e91ab65b3c4dc1cd74dbbb
djs43 <DanScanlan98@gmail.com>
2024-09-27 15:39:31 -0400

    readability updates

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const {Client,Events,GatewayIntentBits,SlashCommandBuilder} = require("discord.js");
-const {token} = require("./config.json");
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require("discord.js");
+const { token } = require("./config.json");
 
-const client = new Client({intents:[GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady,c => {
-    console.log(`Logged in as ${c.user.tag}`)
+client.once(Events.ClientReady, async c => {
+    console.log(`Logged in as ${c.user.tag}`);
 
-    const ping = new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with pong');
+    const commands = [
+        new SlashCommandBuilder()
+            .setName('ping')
+            .setDescription('Replies with pong'),
+        new SlashCommandBuilder()
+            .setName('hello')
+            .setDescription('Says hello to someone!'),
+        new SlashCommandBuilder()
+            .setName('hola')
+            .setDescription('Says hello to someone!')
+    ].map(command => command.toJSON());
 
-    const hello = new SlashCommandBuilder()
-        .setName('hello')
-        .setDescription('Says hello to someone!');
-    
-    const hola = new SlashCommandBuilder()
-        .setName('hola')
-        .setDescription('Says hello to someone!');
-
-
-    client.application.commands.create(ping);
-    client.application.commands.create(hello);
-    client.application.commands.create(hola);
+    try {
+        await client.application.commands.set(commands,"929092597505462362");
+        console.log('Commands registered successfully.');
+    } catch (error) {
+        console.error('Error registering commands:', error);
+    }
 });
 
-client.on(Events.InteractionCreate,interaction => {
-    if(!interaction.isChatInputCommand()) return;
-    if(interaction.commandName === "ping"){
+client.on(Events.InteractionCreate, interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "ping") {
         interaction.reply("Pong!");
     }
-    if(interaction.commandName === "hello"){
+    if (interaction.commandName === "hello") {
         interaction.reply(`Hello ${interaction.user.username}`);
     }
+    if (interaction.commandName === "hola") {
+        interaction.reply(`¡Hola ${interaction.user.username}!`);
+    }
+
     console.log(interaction);
 });
+
 client.login(token);
