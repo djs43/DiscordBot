eb1dc08e85f394e3094a2e0fd6e0506252e8d17b
djs43 <DanScanlan98@gmail.com>
2024-09-27 16:57:17 -0400

    first audio works

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require("discord.js");
 const { token } = require("./config.json");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+    ]
+});
 
 client.once(Events.ClientReady, async c => {
     console.log(`Logged in as ${c.user.tag}`);
@@ -15,28 +22,62 @@ client.once(Events.ClientReady, async c => {
             .setDescription('Says hello to someone!'),
         new SlashCommandBuilder()
             .setName('hola')
-            .setDescription('Says hello to someone!')
+            .setDescription('Says hello to someone!'),
+        new SlashCommandBuilder()
+            .setName('play')
+            .setDescription('Plays a music file')
+            .addStringOption(option =>
+                option.setName('file')
+                    .setDescription('The audio file to play')
+                    .setRequired(true))
     ].map(command => command.toJSON());
 
     try {
-        await client.application.commands.set(commands,"929092597505462362");
+        await client.application.commands.set(commands, "929092597505462362");
         console.log('Commands registered successfully.');
     } catch (error) {
         console.error('Error registering commands:', error);
     }
 });
 
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === "ping") {
-        interaction.reply("Pong!");
+        await interaction.reply("Pong!");
     }
-    if (interaction.commandName === "hello") {
-        interaction.reply(`Hello ${interaction.user.username}`);
+    else if (interaction.commandName === "hello") {
+        await interaction.reply(`Hello ${interaction.user.username}`);
     }
-    if (interaction.commandName === "hola") {
-        interaction.reply(`¡Hola ${interaction.user.username}!`);
+    else if (interaction.commandName === "hola") {
+        await interaction.reply(`¡Hola ${interaction.user.username}!`);
+    }
+    else if (interaction.commandName === "play") {
+        const voiceChannel = interaction.member.voice.channel;
+
+        if (!voiceChannel) {
+            return interaction.reply('You need to be in a voice channel to play music!');
+        }
+
+        const connection = joinVoiceChannel({
+            channelId: voiceChannel.id,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+
+        const audioFile = interaction.options.getString('file');
+        const player = createAudioPlayer();
+        const resource = createAudioResource(audioFile); // Make sure the file is accessible
+
+        player.play(resource);
+        connection.subscribe(player);
+
+        await interaction.reply(`Now playing: ${audioFile}`);
+
+        player.on('idle', () => {
+            connection.destroy();
+            console.log('Left the voice channel.');
+        });
     }
 
     console.log(interaction);
